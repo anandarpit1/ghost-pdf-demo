b9fc31b7c9eb51a21f2d4dfd1bd0448868eb3a30
Arpit Anand <arpit@iof1629d.pnap.ioflood.net>
2022-06-09 21:57:55 +0530

    added pdftk for compression and soffice for ppt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 7025;
+const PORT = process.env.PORT || 5000;
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -34,12 +34,64 @@ const upload = multer({
 
 var pdfUpload = upload.single("file");
 
+
 app.post("/compress", (req, res) => {
   pdfUpload(req, res, function (err) {
     if (err) {
       return res.end("Error uploading file");
     } else {
       var inputFile = req.file.path;
+      console.log(inputFile)
+
+      const inputName = req.file.filename;
+      const deduct = path.extname(inputName).toString();
+
+      const outputFile =
+        inputName.substr(0, inputName.length - deduct.length) +
+        "." 
+        "output" +
+        ".pdf";
+      const outputFilePath = __dirname + "/output/" + outputFile;
+
+      //Using Ghostscript
+      exec(
+        `gs \ -q -dNOPAUSE -dBATCH -dSAFER -dQUIET \ -sDEVICE=pdfwrite \ -dCompatibilityLevel=1.5  \ -dEmbedAllFonts=false \ -dSubsetFonts=false \ -dOptimize=true \ -dDownsampleColorImages=true \ -dDownsampleGrayImages=true \ -dDownsampleMonoImages=true \  -dAutoRotatePages=/None \ -dDetectDuplicateImages=true \ -dCompressFonts=true \ -r10 \ -dCompressFonts \ -dColorImageDownsampleType=/Subsample \ -dColorImageResolution=50 \ -dGrayImageDownsampleType=/Subsample \ -dGrayImageResolution=40 \   -dMonoImageDownsampleType=/Subsample \ -dMonoImageResolution=40 \ -dCompressEntireFile=true\ -sOutputFile="output/${outputFile}" \ ${inputFile}`,
+        (err, stdout, stderr) => {
+          if (err) {
+            return res.json({
+              error: err.message,
+            });
+          } else {
+            return res.sendFile(outputFilePath);
+          }
+        }
+      );
+
+      //Using PDF TK
+      // exec(
+      //   `pdftk ${inputFile} output "output/${outputFile}" compress `,
+      //   (err, stdout, stderr) => {
+      //     if (err) {
+      //       return res.json({
+      //         error: err.message,
+      //       });
+      //     } else {
+      //       return res.sendFile(outputFilePath);
+      //     }
+      //   }
+      // );
+    }
+  });
+});
+
+
+app.post("/pdftoppt", (req, res) => {
+  pdfUpload(req, res, function (err) {
+    if (err) {
+      return res.end("Error uploading file");
+    } else {
+      var inputFile = req.file.path;
+      console.log(inputFile)
 
       const inputName = req.file.filename;
       const deduct = path.extname(inputName).toString();
@@ -51,19 +103,19 @@ app.post("/compress", (req, res) => {
         ".pdf";
       const outputFilePath = __dirname + "/output/" + outputFile;
 
+      //Using Libre office
       exec(
-        `gs \ -q -dNOPAUSE -dBATCH -dSAFER \ -sDEVICE=pdfwrite \ -dCompatibilityLevel=1.5  \ -dEmbedAllFonts=false \ -dSubsetFonts=true \ -dOptimize=true \
-        -dDownsampleColorImages=true \ -dDownsampleGrayImages=true \ -dDownsampleMonoImages=true \ -dAutoRotatePages=/None \ -dCompressFonts=true \ -dColorImageDownsampleType=/Bicubic \ -dColorImageResolution=50 \ -dGrayImageDownsampleType=/Bicubic \ -dGrayImageResolution=40 \ -dMonoImageDownsampleType=/Bicubic \ -dMonoImageResolution=40 \ -sOutputFile="output/${outputFile}" \ ${inputFile}`,
-        (err, stdout, stderr) => {
-          if (err) {
-            return res.json({
-              error: err.message,
-            });
-          } else {
-            return res.sendFile(outputFilePath);
+          `soffice --infilter=impress_pdf_import --convert-to ppt ${inputFile} --outdir "output/${outputFile}"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              return res.json({
+                error: err.message,
+              });
+            } else {
+              return res.sendFile(outputFilePath);
+            }
           }
-        }
-      );
+        );
     }
   });
 });
